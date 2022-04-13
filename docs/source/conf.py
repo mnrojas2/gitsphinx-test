78c0bf1bdfe5a5fe3dc3fd13771ef709b1555d12
MnRojas <68394846+mnrojas2@users.noreply.github.com>
2022-04-13 19:40:41 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,8 @@
 # add these directories to sys.path here.
 import pathlib
 import sys
-import furo
+#import furo
+import sphinx_bootstrap_theme
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 
@@ -56,7 +57,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_bootstrap_theme'
+#html_theme = 'furo'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
